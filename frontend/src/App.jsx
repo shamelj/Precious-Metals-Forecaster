17372fea2fb53a8d998c6cf3e2a0e063a80877a4
Omar Abbadi <omar.abbadi2015@gmail.com>
2023-09-27 09:29:45 +0300

    configuering Router and adding evaluation page to it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ContinuousEvaluation from "./Pages/ContinuousEvaluation"
+import Home from "./Pages/Home";
+import NavBar from "./Components/NavBar"
+
 const App = () => {
   return (
-    <div>
-      <ContinuousEvaluation/>
-    </div>
-  // <BrowserRouter>
-  // <Routes>
-  //   <Route>
-  //     index
-  //     path="/"
-  //     element={ <Home /> }
-  //   </Route>
-  // </Routes>
-  // </BrowserRouter>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/continuous_evaluation" element={<ContinuousEvaluation />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/continuous_evaluation">Evaluation</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
