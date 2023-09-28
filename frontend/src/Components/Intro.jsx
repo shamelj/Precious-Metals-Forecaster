c46df4f9cb1e15fc6c1d1fcf887f04429eee13b9
Omar Abbadi <omar.abbadi2015@gmail.com>
2023-09-28 20:12:20 +0300

    using variables for colors in css

--- frontend/src/Components/Intro.jsx
+++ frontend/src/Components/Intro.jsx
@@ -4,14 +4,11 @@ const Intro = () => {
         <div className="intro">
             <h2>About the Project</h2>
             <p className="intro-paragraph">
-                This project is a groundbreaking endeavor in the field of data science and artificial intelligence. Our team, consisting of Omar Abbadi, Shamel Juidy, Moawya Abdulrahman, and Taha Nasassra, has developed a cutting-edge AI model for predicting gold prices. This model utilizes advanced machine learning techniques to analyze historical gold price data and forecast future prices with remarkable accuracy.
+                This project is a groundbreaking endeavor in the field of data science and artificial intelligence. Our team, consisting of Omar Abbadi, Taha Nassasra, Shamel Juidy, and Moawyah Abdulrahman, has developed a cutting-edge AI model for predicting gold prices. This model utilizes advanced machine learning techniques to analyze historical gold price data and forecast future prices with remarkable accuracy.
             </p>
             <p className="intro-paragraph">
                 The significance of this project lies in its potential to empower investors, financial institutions, and businesses by providing them with invaluable insights into the volatile gold market. Gold prices have a profound impact on global economies, making accurate predictions a game-changer in decision-making processes.
             </p>
-            {/* <p className="intro-paragraph">
-                Our journey began as part of the data science training program facilitated by Gaza Sky Geeks, a renowned tech hub in the region. Under the guidance of our dedicated trainer, Asem Saleh, and the co-instructors Abdullah Qaradeh and Maysam Mousa we embarked on this challenging yet rewarding final project.
-            </p> */}
         </div>
     );
 };
