b213fe51edf577b975e096e2bacb94c45264b7b5
shamelj <shamel88552@gmail.com>
2023-09-29 18:27:19 +0300

    Added seed with predictions

--- backend/Predictor.py
+++ backend/Predictor.py
@@ -5,15 +5,12 @@ import config
 import joblib
 class Predictor:
     def __init__(self):
-        print(config.Predictor.MODEL_PATH)
         self.model = load_model(config.Predictor.MODEL_PATH)
         self.x_scaler = joblib.load(config.Predictor.X_SCALER_PATH)
         self.y_scaler = joblib.load(config.Predictor.Y_SCALER_PATH)
 
     def predict(self, price_sequence): # expecting 1d np array
         price_sequence_scaled = self.x_scaler.transform(price_sequence.reshape(-1,1))
-        print(f"scaled :{price_sequence_scaled}")
         prediction = self.model.predict(price_sequence_scaled.reshape(-1,5,1), verbose=0)
-        print(prediction)
         prediction_unscaled = self.y_scaler.inverse_transform(prediction.reshape(-1, 1))
-        return prediction_unscaled
+        return float(prediction_unscaled[0][0])
